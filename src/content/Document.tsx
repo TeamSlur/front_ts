85d41dc2e40455d015d57bc99380aa24a053d599
SeungMoYu <smy0917@naver.com>
2024-06-07 22:24:17 +0900

    [CHORE] workspace page rendering

--- src/content/Document.tsx
+++ src/content/Document.tsx
@@ -1,18 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Document.scss';
 
-const Document: React.FC = () => {
-    const [content, setContent] = useState("여기에 문서 내용을 작성하세요.");
-
-    const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setContent(event.target.value);
-    };
+const Document = () => {
 
     return (
-        <div className="documentPage">
-            <h1>문서 페이지</h1>
-            <textarea value={content} onChange={handleChange} />
-        </div>
+        <div>document</div>
     );
 };
 
