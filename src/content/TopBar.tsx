187bd4686a7a222d6e90bd28c8242fa70235d636
SeungMoYu <smy0917@naver.com>
2024-06-07 22:22:20 +0900

    [FEAT] add chat container

--- src/content/TopBar.tsx
+++ src/content/TopBar.tsx
@@ -5,18 +5,27 @@ import SettingIcon from "../component/SettingIcon";
 import AlertIcon from "../component/AlertIcon";
 import './TopBar.scss'
 
-const TopBar: React.FC = () => {
+interface Props {
+    toggleChat: () => void;
+}
+
+const TopBar: React.FC<Props> = ({ toggleChat }) => {
     return (
-        <div className={'topBar'}>
-            <div className={'LogoContainer'}>
-                <img id="Logo_login" src={`${process.env.PUBLIC_URL}/assets/imgs/logo.png`} alt="Logo" className={'topBarLogo'}/>
-                <span className={'teamName'}>TeamSlur</span>
+        <div className="topBar">
+            <div className="LogoContainer">
+                <img
+                    id="Logo_login"
+                    src={`${process.env.PUBLIC_URL}/assets/imgs/logo.png`}
+                    alt="Logo"
+                    className="topBarLogo"
+                />
+                <span className="teamName">TeamSlur</span>
             </div>
-            <div className={'topBarIcons'}>
-                <ChatIcon/>
-                <AlertIcon/>
-                <UserIcon/>
-                <SettingIcon/>
+            <div className="topBarIcons">
+                <ChatIcon onClick={toggleChat} />
+                <AlertIcon />
+                <UserIcon />
+                <SettingIcon />
             </div>
         </div>
     );
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,35 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TopBar from "../content/TopBar";
 import SideBar from "../content/SideBar";
 import Document from "../content/Document";
 import TestPage from "./TestPage";
 import {useParams} from "react-router-dom";
+import './Main.scss'
 
 const Main=()=>{
     const {pageContent} = useParams<{ pageContent: string }>();
+    const [showChat, setShowChat] = useState(false);
     const renderPage: { [key: string]: JSX.Element } = {
         'document': <Document />,
         'test': <TestPage />,
         'default' : <Document />
     };
-
+    const toggleChat = () => {
+        setShowChat(!showChat);
+    };
     const getPageContent = (page: string | undefined): JSX.Element => {
         if (page && renderPage[page]) {
             return renderPage[page];
         }
         return renderPage['default'];
     };
-    console.log(getPageContent(pageContent))
+
     return (
         <div className="mainContainer">
-            <TopBar />
-            <div className="contentContainer">
+            <TopBar toggleChat={toggleChat}/>
+            <div className={`contentContainer ${showChat ? 'chatVisible' : ''}`}>
                 <SideBar pageContent={'default'}/>
                 <div className="pageContent">
                     {getPageContent(pageContent)}
                 </div>
+                {showChat && <div className="chatContainer">Chat Content</div>}
             </div>
         </div>
+
     );
 }
 export default Main
